66d5113e76d94ee7fc309e715cc94ef585b55442
Anniele Oliveiraa <annieleoliveiraa@gmail.com>
2024-09-30 16:05:38 -0300

    Adicionando o js do proejto todolist

--- todoList/js/scripts.js
+++ todoList/js/scripts.js
@@ -1,7 +1,13 @@
 // Clase
 
 class ToDo {
-
+  Texto 
+  Prioridade 
+  Feito = false 
+  constructor(texto, Prioridade) {
+   this.Texto = texto
+   this.Prioridade = Prioridade
+  }
 }
 
 // Array
@@ -9,31 +15,69 @@ class ToDo {
 
 //funções projeto
 
-function CriarToDo() {
-
+function CriarToDo(texto, Prioridade, Array) {
+ let objetoTodo = new ToDo(texto, Prioridade)
+ if(!Array.some(x => x.Texto == texto))
+  array.push(objetoTodo)
+ return objetoTodo
 }
 
-function AtualizarToDo() {
-
+function AtualizarToDo(textoAntigo, textoNovo, array) {
+ let atualizado = false
+ array.forEach(todo => {
+  if(todo.Texto == textoAntigo) {
+    todo.Texto = textoNovo
+    atualizado = true 
+  }
+ })
+ return atualizado
 }
 
-function ConcluirToDo() {
-
+function ConcluirToDo(array, texto) {
+ let concluido = false 
+ array.forEach(todo => {
+  if(todo.Texto == texto) {
+    if(todo.Feito)
+      todo.Feito = false 
+    else {
+      todo.Feito = true 
+    }
+    concluido = true
+  }
+ })
+ return concluido
 }
 
-function ExcluirToDo() {
-
+function ExcluirToDo(array, texto) {
+ let index
+ let removido = false 
+ array.forEach(todo => {
+  if(todo.Texto == texto) {
+    index = array.indexOf(todo)
+    removido = true
+  }
+ })
+ array.splice(index, 1)
+ return removido 
 }
 
-function PesquisarToDo() {
- 
+function PesquisarToDo(array, texto) {
+  let pesquisa = false 
+  array.forEach(todo => {
+    if(todo.Texto.includes(texto)) {
+      pesquisa = true 
+    }
+  })
+  return pesquisa 
 }
 
-function OrdenarCrescente() {
-  
+function OrdenarCrescente(array) {
+  array.sort((a, b) => a.Prioridade - b.Prioridade)
+  return array 
 }
-function OrdenarDecrescente() {
-  
+function OrdenarDecrescente(array) {
+  array.sort((a, b) => b.Prioridade - a.Prioridade)
+  return array 
 }
 
 // Seleção de elementos
